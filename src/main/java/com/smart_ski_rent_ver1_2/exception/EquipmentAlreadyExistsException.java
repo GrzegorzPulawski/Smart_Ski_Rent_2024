60e66269b3a9b76288dfbbe5f93ff6eecfb5d827
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2024-08-03 09:12:00 +0200

    feature(task1.3): implemented EquipmentService

---
+++ src/main/java/com/smart_ski_rent_ver1_2/exception/EquipmentAlreadyExistsException.java
@@ -0,0 +1,4 @@
+package com.smart_ski_rent_ver1_2.exception;
+
+public class EquipmentAlredyExistsException extends RuntimeException{
+}
---
+++ src/main/java/com/smart_ski_rent_ver1_2/exception/EquipmentNotExists.java
@@ -0,0 +1,2 @@
+package com.smart_ski_rent_ver1_2.exception;public class EquipmentNotExists {
+}
