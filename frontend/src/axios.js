7767fe1a9b00fdf2e2f7002c537936a0e94b578c
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2024-10-15 18:00:47 +0200

    upgrade axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const connection = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "/"
 });
 
 // Interceptor dodający nagłówek Authorization dla Basic Auth
