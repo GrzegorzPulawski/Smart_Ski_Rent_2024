33442ea2a20d55c934e961fb1047d950f6c9b269
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2024-10-17 15:33:47 +0200

    update befor deploy2

--- src/main/java/com/smart_ski_rent_ver1_2/Application.java
+++ src/main/java/com/smart_ski_rent_ver1_2/Application.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
   public class Application {
 
-    public static void main(String[] args)
-
-    {
-        System.setProperty("server.port", System.getenv("PORT") != null ? System.getenv("PORT") : "8080");
-        SpringApplication.run(Application.class, args);}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
