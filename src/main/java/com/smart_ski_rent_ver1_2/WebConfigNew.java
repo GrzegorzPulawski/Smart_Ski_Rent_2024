79ba3cf4b959c6357b65f8e7203d3345cfdd8632
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2024-11-09 11:36:35 +0100

    ddResourceHandlers i WebConfig

--- src/main/java/com/smart_ski_rent_ver1_2/WebConfigNew.java
+++ src/main/java/com/smart_ski_rent_ver1_2/WebConfigNew.java
@@ -1,20 +1,25 @@
 package com.smart_ski_rent_ver1_2;
 
-import io.swagger.models.HttpMethod;
+//import io.swagger.models.HttpMethod;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.http.HttpHeaders;
-import java.util.Arrays;
-@EnableWebMvc
-@Configuration
-public class WebConfigNew {
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.Arrays;
+
+@Configuration
+@EnableWebMvc
+public class WebConfigNew implements WebMvcConfigurer {
+
+    // CORS Configuration
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -32,9 +37,15 @@ public class WebConfigNew {
                 HttpMethod.PUT.name(),
                 HttpMethod.DELETE.name()
         ));
-        config.setMaxAge(3600L); // Set max age for preflight requests
+        config.setMaxAge(3600L);
         source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
-        return new CorsFilter(source); // Directly return CorsFilter instead of FilterRegistrationBean
+    // Resource Handling for Static Files (e.g. React Build)
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
