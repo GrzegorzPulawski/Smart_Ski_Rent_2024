fb4c191e20f0f235b05338f9f3cf40609379e04a
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2024-11-10 16:25:39 +0100

    newDEPLOY4 4

--- src/main/java/com/smart_ski_rent_ver1_2/WebConfigNew.java
+++ src/main/java/com/smart_ski_rent_ver1_2/WebConfigNew.java
@@ -1,31 +1,27 @@
 package com.smart_ski_rent_ver1_2;
 
-//import io.swagger.models.HttpMethod;
+import io.swagger.models.HttpMethod;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.CorsConfiguration;
-
+import org.springframework.http.HttpHeaders;
 import java.util.Arrays;
-
-@Configuration
 @EnableWebMvc
-public class WebConfigNew implements WebMvcConfigurer {
+@Configuration
+public class WebConfigNew {
 
-    // CORS Configuration
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://frontend-smart-ski-ed51f9e974f1.herokuapp.com/");
+        config.addAllowedOrigin("http://localhost:3000"); // Allow specific origin
+        config.addAllowedOrigin("https://frontend-smart-ski-ed51f9e974f1.herokuapp.com");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
@@ -37,15 +33,9 @@ public class WebConfigNew implements WebMvcConfigurer {
                 HttpMethod.PUT.name(),
                 HttpMethod.DELETE.name()
         ));
-        config.setMaxAge(3600L);
+        config.setMaxAge(3600L); // Set max age for preflight requests
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 
-    // Resource Handling for Static Files (e.g. React Build)
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
+        return new CorsFilter(source); // Directly return CorsFilter instead of FilterRegistrationBean
     }
 }
